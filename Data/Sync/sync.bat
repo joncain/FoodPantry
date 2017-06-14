c354c9b0505e83541ff1175dd78fd6818db1c854
joncain <jon@joncain.net>
2017-06-14 13:25:43 -0600

    Refactored to use variables

--- Data/Sync/sync.bat
+++ Data/Sync/sync.bat
@@ -3,17 +3,24 @@
 ECHO Starting Job
 
 ECHO Removing *_output.txt files
-DEL "*_output.txt"
+DEL *_output.txt
+
+SET step1=1_backup_recipients_table
+SET step2=2_extract_new_recipients_table
+SET step3=3_dump_recipients_schema
+SET step4=4_drop_recipients_table
+SET step5=5_create_recipients_table
+SET step6=6_import_new_recipients_table
 
 REM ------------------------------------------------------------------------
 ECHO Step #1 - Backup existing Recipients table
 
-sqlite3 "../FoodPantry.db" ".read 1_backup_recipients_table.txt"
+sqlite3 "../FoodPantry.db" ".read %step1%.txt"
 
 ECHO Verify output file
 
-IF NOT EXIST 1_backup_recipients_table.txt (
-	ECHO ERROR! Output file 1_backup_recipients_table.txt could not be verified - exiting
+IF NOT EXIST %step1%_output.txt (
+	ECHO ERROR! Output file %step1%_output.txt could not be verified - exiting
 	EXIT /B
 )
 
@@ -29,12 +36,12 @@ IF NOT EXIST NewFoodPantry.db (
 	EXIT /B
 )
 
-sqlite3 NewFoodPantry.db ".read 2_extract_new_recipients_table.txt"
+sqlite3 NewFoodPantry.db ".read %step2%.txt"
 
 ECHO Verify output file
 
-IF NOT EXIST 2_extract_new_recipients_table_output.txt (
-	ECHO ERROR! Output file could not be verified - exiting
+IF NOT EXIST %step2%_output.txt (
+	ECHO ERROR! Output file %step2%_output.txt could not be found - exiting
 	EXIT /B
 )
 
@@ -45,12 +52,12 @@ REM ------------------------------------------------------------------------
 REM ------------------------------------------------------------------------
 ECHO Step #3 - Dump Recipients schema
 
-sqlite3 "../FoodPantry.db" ".read 3_dump_recipients_schema.txt"
+sqlite3 "../FoodPantry.db" ".read %step3%.txt"
 
 ECHO Verify output file
 
-IF NOT EXIST 3_dump_recipients_schema_output.txt (
-	ECHO ERROR! Output file could not be verified - exiting
+IF NOT EXIST %step3%_output.txt (
+	ECHO ERROR! Output file %step3%_output.txt could not be found - exiting
 	EXIT /B
 )
 
@@ -61,7 +68,7 @@ REM ------------------------------------------------------------------------
 REM ------------------------------------------------------------------------
 ECHO Step #4 - Drop Recipients table
 
-sqlite3 "../FoodPantry.db" ".read 4_drop_recipients_table.txt"
+sqlite3 "../FoodPantry.db" ".read %step4%.txt"
 
 ECHO Step #4 Complete
 REM ------------------------------------------------------------------------
@@ -70,7 +77,7 @@ REM ------------------------------------------------------------------------
 REM ------------------------------------------------------------------------
 ECHO Step #5 - Create Recipients table
 
-sqlite3 "../FoodPantry.db" ".read 5_create_recipients_table.txt"
+sqlite3 "../FoodPantry.db" ".read %step5%.txt"
 
 ECHO Step #5 Complete
 REM ------------------------------------------------------------------------
@@ -79,11 +86,10 @@ REM ------------------------------------------------------------------------
 REM ------------------------------------------------------------------------
 ECHO Step #6 - Import New Recipients table
 
-sqlite3 "../FoodPantry.db" ".read 6_import_new_recipients_table.txt"
+sqlite3 "../FoodPantry.db" ".read %step6%.txt"
 
 ECHO Step #6 Complete
 REM ------------------------------------------------------------------------
-
 
 
 ECHO Job Complete
